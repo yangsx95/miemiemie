68adb819e3f9cd3364e8b13343c1b3f28448c330
杨顺翔 <616510229@qq.com>
2023-01-18 11:08:54 +0800

    feat(validate): add utils

---
+++ miemiemie-starter-validation/src/main/java/com/miemiemie/starter/validation/util/CommonValidatorGroup.java
@@ -0,0 +1,46 @@
+package com.miemiemie.starter.validation.util;
+
+import javax.validation.groups.Default;
+
+/**
+ * @author yangshunxiang
+ * @since 2023/1/18
+ */
+public interface CommonValidatorGroup {
+
+    /**
+     * 新增校验组
+     */
+    interface Add extends Default {
+    }
+
+    /**
+     * 更新校验组
+     */
+    interface Update extends Default {
+    }
+
+    /**
+     * 删除校验组
+     */
+    interface Delete extends Default {
+    }
+
+    /**
+     * 通用单挑查询校验组
+     */
+    interface SelectOne extends Default {
+    }
+
+    /**
+     * 通用多条查询校验组
+     */
+    interface SelectList extends Default {
+    }
+
+    /**
+     * 有效数据查询校验组
+     */
+    interface SelectEnableList extends Default {
+    }
+}
