df632fc38e171316bacfccc036e4b9d8f5b36860
杨顺翔 <yangsx95@qq.com>
2023-01-24 22:52:03 +0800

    feat(web): jackson 其他配置

--- miemiemie-starter-web/src/main/java/com/miemiemie/starter/web/jackson/JacksonConfig.java
+++ miemiemie-starter-web/src/main/java/com/miemiemie/starter/web/jackson/JacksonConfig.java
@@ -1,11 +1,15 @@
 package com.miemiemie.starter.web.jackson;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.miemiemie.core.enums.CommonEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.context.annotation.Configuration;
+
+import java.text.SimpleDateFormat;
 
 /**
  * jackson相关配置
@@ -26,5 +30,11 @@ public class JacksonConfig implements SmartInitializingSingleton {
         simpleModule.addSerializer(CommonEnum.class, new CommonEnumSerializer());
         simpleModule.addDeserializer(CommonEnum.class, new CommonEnumDeserializer());
         objectMapper.registerModule(simpleModule);
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        // 默认情况下，解析json为对象时，如果json中的某个属性在对象中不存在，会引发报错
+        // 使用disable禁用此特性
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        // 设置不忽略空的字段
+        objectMapper.setSerializationInclusion(JsonInclude.Include.ALWAYS);
     }
 }
