bb468654313ef558aad9075c774255aae78bd3b3
杨顺翔 <616510229@qq.com>
2023-01-31 18:09:45 +0800

    feat(脱敏): 增加脱敏注解

---
+++ miemiemie-springboot-starter/miemiemie-starter-desensitization/src/main/java/com/miemiemie/starter/desensitization/anno/Insensitive.java
@@ -0,0 +1,10 @@
+package com.miemiemie.starter.desensitization.anno;
+
+/**
+ * 不敏感标记，被标记目标字段代表此字段的值时公开的，不敏感的，如果传入值为敏感值，则会进行反脱敏操作
+ *
+ * @author yangshunxiang
+ * @since 2023/1/31
+ */
+public @interface Insensitive {
+}
---
+++ miemiemie-springboot-starter/miemiemie-starter-desensitization/src/main/java/com/miemiemie/starter/desensitization/anno/Sensitive.java
@@ -0,0 +1,10 @@
+package com.miemiemie.starter.desensitization.anno;
+
+/**
+ * 敏感标记，被修饰的类型、字段
+ *
+ * @author yangshunxiang
+ * @since 2023/1/31
+ */
+public @interface Sensitive {
+}
