f1aaf1168dfdb630157eda8ec8b2702bebe81f22
杨顺翔 <616510229@qq.com>
2023-03-09 19:14:43 +0800

    refactor: 更改项目结构

--- miemiemie-starters/miemiemie-openapi/src/main/java/com/miemiemie/starter/openapi/OpenApiProperties.java
+++ miemiemie-starters/miemiemie-openapi/src/main/java/com/miemiemie/starter/openapi/OpenApiProperties.java
@@ -8,6 +8,8 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import static springfox.documentation.spring.web.plugins.Docket.DEFAULT_GROUP_NAME;
+
 /**
  * @author yangshunxiang
  * @since 2023/1/19
@@ -16,33 +18,127 @@ import java.util.Map;
 @ConfigurationProperties(prefix = "miemiemie.openapi")
 public class OpenApiProperties {
 
-    /**
-     * 标题
-     **/
-    private String title = "在线文档";
-    private String group = "";
-    /**
-     * 描述
-     **/
-    private String description = "lamp-admin-cloud 在线文档";
-    /**
-     * 版本
-     **/
-    private String version = "1.0";
-    /**
-     * 许可证
-     **/
-    private String license = "";
-    /**
-     * 许可证URL
-     **/
-    private String licenseUrl = "";
-    /**
-     * 服务条款URL
-     **/
-    private String termsOfServiceUrl = "";
+    private List<DocketInfo> docketInfoList = new ArrayList<>();
 
-    private Contact contact = new Contact();
+    @Data
+    public static class DocketInfo {
+
+        /**
+         * 是否启用
+         */
+        private boolean enabled = true;
+
+        /**
+         * 组名
+         */
+        private String groupName = DEFAULT_GROUP_NAME;
+
+        /**
+         * host信息
+         **/
+        private String host = "";
+
+        /**
+         * Api基本信息
+         */
+        private ApiInfo apiInfo = new ApiInfo();
+
+        /**
+         * API选择器
+         */
+        private Selector selector = new Selector();
+
+        /**
+         * swagger会解析的包路径
+         **/
+        private String basePackage = "";
+        private String basePath = "/";
+        /**
+         * swagger会解析的url规则
+         **/
+        private List<String> includePath = new ArrayList<>();
+
+        /**
+         * 在includePath基础上需要排除的url规则
+         **/
+        private List<String> excludePath = new ArrayList<>();
+
+        private List<GlobalOperationParameter> globalOperationParameters;
+        /**
+         * 全局统一鉴权配置
+         **/
+        private Authorization authorization;
+        /**
+         *
+         */
+        private List<ApiKey> apiKeys = new ArrayList<>();
+
+        /**
+         * 排序
+         */
+        private Integer order = 1;
+
+    }
+
+    @Data
+    public static class ApiInfo {
+        /**
+         * 文档版本号
+         */
+        private final String version = "1.0";
+        /**
+         * 文件标题
+         */
+        private final String title = "API文档";
+        /**
+         * 文档描述
+         */
+        private final String description = "MIEMIEMIE API文档";
+        /**
+         * 服务条款URL
+         **/
+        private final String termsOfServiceUrl = "";
+        /**
+         * 许可证
+         */
+        private final String license = "";
+        /**
+         * 许可证url
+         */
+        private final String licenseUrl = "";
+
+        /**
+         * 联系人信息
+         */
+        private final Contact contact = new Contact();
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Contact {
+        /**
+         * 联系人
+         **/
+        private String name = "";
+        /**
+         * 联系人url
+         **/
+        private String url = "";
+        /**
+         * 联系人email
+         **/
+        private String email = "";
+
+    }
+
+    /**
+     * Api选择器
+     */
+    public static class Selector {
+
+    }
 
     /**
      * swagger会解析的包路径
@@ -52,10 +148,12 @@ public class OpenApiProperties {
      * 扩展swagger 基础路径
      */
     private String basePath = "/";
+
     /**
      * SpringSecurity 全局统一鉴权配置
      **/
     private Authorization authorization;
+
     /**
      *
      */
@@ -76,10 +174,7 @@ public class OpenApiProperties {
      **/
     private Map<String, DocketInfo> docket = new LinkedHashMap<>();
 
-    /**
-     * host信息
-     **/
-    private String host = "";
+
 
     /**
      * 排序
@@ -91,17 +186,11 @@ public class OpenApiProperties {
      **/
     private List<GlobalOperationParameter> globalOperationParameters;
 
-    public String getGroup() {
-        if (group == null || "".equals(group)) {
-            return title;
-        }
-        return group;
-    }
-
-
     @Setter
     @Getter
     public static class GlobalOperationParameter {
+
+
         /**
          * 参数名
          **/
@@ -116,12 +205,10 @@ public class OpenApiProperties {
          * 指定参数类型
          **/
         private String modelRef = "String";
-
         /**
          * 参数放在哪个地方:header,query,path,body.form
          **/
         private String parameterType = "header";
-
         /**
          * 参数是否必须传
          **/
@@ -143,82 +230,13 @@ public class OpenApiProperties {
          * 排序
          */
         private int order = 1;
-    }
 
-    @Data
-    public static class DocketInfo {
-        /**
-         * 标题
-         **/
-        private String title = "在线文档";
-        /**
-         * 自定义组名
-         */
-        private String group = "";
-        /**
-         * 描述
-         **/
-        private String description = "lamp-admin-cloud 在线文档";
-        /**
-         * 版本
-         **/
-        private String version = "";
-        /**
-         * 许可证
-         **/
-        private String license = "";
-        /**
-         * 许可证URL
-         **/
-        private String licenseUrl = "";
-        /**
-         * 服务条款URL
-         **/
-        private String termsOfServiceUrl = "";
-
-        private Contact contact = new Contact();
-
-        /**
-         * swagger会解析的包路径
-         **/
-        private String basePackage = "";
-
-        private String basePath = "/";
-        /**
-         * swagger会解析的url规则
-         **/
-        private List<String> includePath = new ArrayList<>();
-        /**
-         * 在includePath基础上需要排除的url规则
-         **/
-        private List<String> excludePath = new ArrayList<>();
-
-        private List<GlobalOperationParameter> globalOperationParameters;
-
-        /**
-         * 全局统一鉴权配置
-         **/
-        private Authorization authorization;
-        /**
-         *
-         */
-        private List<ApiKey> apiKeys = new ArrayList<>();
-        /**
-         * 排序
-         */
-        private Integer order = 1;
-
-        public String getGroup() {
-            if (group == null || "".equals(group)) {
-                return title;
-            }
-            return group;
-        }
     }
 
     @Data
     @NoArgsConstructor
     public static class Authorization {
+
 
         /**
          * 鉴权策略ID，需要和SecurityReferences ID保持一致
@@ -234,13 +252,14 @@ public class OpenApiProperties {
          * 鉴权作用域列表
          */
         private List<AuthorizationScope> authorizationScopeList = new ArrayList<>();
-
         private List<String> tokenUrlList = new ArrayList<>();
+
     }
 
     @Data
     @NoArgsConstructor
     public static class AuthorizationScope {
+
 
         /**
          * 作用域名称
@@ -252,24 +271,6 @@ public class OpenApiProperties {
          */
         private String description = "";
 
-    }
-
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class Contact {
-        /**
-         * 联系人
-         **/
-        private String name = "";
-        /**
-         * 联系人url
-         **/
-        private String url = "";
-        /**
-         * 联系人email
-         **/
-        private String email = "";
     }
 
     @Data
