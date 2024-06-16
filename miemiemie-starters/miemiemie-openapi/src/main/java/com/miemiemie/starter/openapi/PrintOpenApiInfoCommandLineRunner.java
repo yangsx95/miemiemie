30d66e811cd18448b8e01bdd8de4448a92b8007b
杨顺翔 <yangsx95@qq.com>
2024-06-16 18:38:09 +0800

    修复问题

--- miemiemie-starters/miemiemie-openapi/src/main/java/com/miemiemie/starter/openapi/PrintOpenApiInfoCommandLineRunner.java
+++ miemiemie-starters/miemiemie-openapi/src/main/java/com/miemiemie/starter/openapi/PrintOpenApiInfoCommandLineRunner.java
@@ -1,18 +1,21 @@
 package com.miemiemie.starter.openapi;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.service.OpenAPIService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+import org.springframework.context.ApplicationContext;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Objects;
 
 /**
  * @author 杨顺翔
  * @since 2024/6/16
  */
-@Component
 @Slf4j
 public class PrintOpenApiInfoCommandLineRunner implements CommandLineRunner {
 
@@ -25,16 +28,25 @@ public class PrintOpenApiInfoCommandLineRunner implements CommandLineRunner {
     @Value("${springdoc.swagger-ui.path:/swagger-ui/index.html}")
     private String swaggerPath;
 
+    @Resource
+    private ApplicationContext applicationContext;
+
     @Override
     public void run(String... args) {
-        String ip;
         try {
-             ip = InetAddress.getLocalHost().getHostAddress();
-        } catch (UnknownHostException e) {
-            ip = "127.0.0.1";
-        }
+            applicationContext.getBean(OpenAPIService.class);
 
-        log.info("Swagger UI is available at http://{}:{}{}{}", ip, port, contextPath, swaggerPath);
+            String ip;
+            try {
+                ip = InetAddress.getLocalHost().getHostAddress();
+            } catch (UnknownHostException e) {
+                ip = "127.0.0.1";
+            }
+
+            log.info("Swagger UI is available at http://{}:{}{}{}", ip, port, contextPath, swaggerPath);
+        } catch (Exception e) {
+            log.info("Swagger UI not enable");
+        }
     }
 
 
