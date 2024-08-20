defb2440e6e0caed9bd66d425b1ccdd131c4b003
杨顺翔 <yangsx95@qq.com>
2024-08-20 17:39:31 +0800

    feat: mybatis数据库类型获取不正确、分页不生效

--- miemiemie-starters/miemiemie-mybatisplus/src/main/java/com/miemiemie/starter/mybatisplus/MybatisPlusAutoConfig.java
+++ miemiemie-starters/miemiemie-mybatisplus/src/main/java/com/miemiemie/starter/mybatisplus/MybatisPlusAutoConfig.java
@@ -1,11 +1,15 @@
 package com.miemiemie.starter.mybatisplus;
 
+import cn.hutool.db.ds.pooled.DbConfig;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.annotation.FieldStrategy;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.autoconfigure.MybatisPlusProperties;
 import com.baomidou.mybatisplus.autoconfigure.MybatisPlusPropertiesCustomizer;
+import com.baomidou.mybatisplus.core.MybatisConfiguration;
 import com.baomidou.mybatisplus.core.config.GlobalConfig;
 import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.miemiemie.starter.mybatisplus.config.GenericEnumTypeHandler;
@@ -16,7 +20,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @AutoConfiguration
 public class MybatisPlusAutoConfig {
@@ -26,7 +34,17 @@ public class MybatisPlusAutoConfig {
 
     @Bean
     public DbType dbType() {
-        return DbType.getDbType(jdbcUrl);
+        return DbType.getDbType(extractDatabaseType(jdbcUrl));
+    }
+
+    private String extractDatabaseType(String jdbcUrl) {
+        Pattern pattern = Pattern.compile("jdbc:(\\w+):");
+        Matcher matcher = pattern.matcher(jdbcUrl);
+        if (matcher.find()) {
+            return matcher.group(1);
+        } else {
+            return "";
+        }
     }
 
     /**
@@ -50,8 +68,8 @@ public class MybatisPlusAutoConfig {
         PaginationInnerInterceptor pageInterceptor = new PaginationInnerInterceptor();
         // 页溢出不进行处理
         pageInterceptor.setOverflow(Boolean.FALSE);
-        // 单页限制最大1000
-        pageInterceptor.setMaxLimit(1000L);
+        // 单页不做限制
+        pageInterceptor.setMaxLimit(-1L);
         pageInterceptor.setDbType(dbType);
         return pageInterceptor;
     }
@@ -88,6 +106,14 @@ public class MybatisPlusAutoConfig {
             configuration.setLogImpl(org.apache.ibatis.logging.stdout.StdOutImpl.class);
             properties.setConfiguration(configuration);
         };
+    }
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(PaginationInnerInterceptor paginationInnerInterceptor,
+                                                         OptimisticLockerInnerInterceptor optimisticLockerInnerInterceptor) {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.setInterceptors(List.of(paginationInnerInterceptor, optimisticLockerInnerInterceptor));
+        return mybatisPlusInterceptor;
     }
 
     @Bean
